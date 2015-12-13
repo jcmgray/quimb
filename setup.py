f62b64a02dd45c2acd15322ac7b495e50f63a6b6
Johnnie Gray <john.gray.14@ucl.ac.uk>
2015-12-13 22:18:07 +0000

    Update dependency versions required
    Dependencies will now be satisfied by latest anaconda python
    distribution.

--- setup.py
+++ setup.py
@@ -7,12 +7,11 @@ setup(
     license='MIT',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
-        'numpy>=1.10',
-        'numexpr',
-        'scipy',
-        'numba',
-        'numexpr',
-        'matplotlib'
+        'numpy>=1.10.1',
+        'numexpr>=2.4.4',
+        'scipy>=0.16.0',
+        'numba>=0.22.1',
+        'matplotlib>=1.5.0'
         ],
     classifiers=[
         'Development Status :: 3 - Alpha',
