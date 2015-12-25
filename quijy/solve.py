ace0b2b49a62d4cb42417c266e74a656ee2e0c3a
Johnnie Gray <john.gray.14@ucl.ac.uk>
2015-12-25 19:35:31 +0000

    refinements to solve module

--- quijy/solve.py
+++ quijy/solve.py
@@ -9,13 +9,15 @@ import scipy.sparse.linalg as spla
 from quijy.core import qonvert
 
 
+# full eigendecomposition methods for dense matrices #
+# -------------------------------------------------- #
 def eigsys(a, sort=True):
-    """ Find sorted eigenpairs of matrix
+    """ Find all eigenpairs of dense matrix
     Input:
         a: hermitian matrix
         sort: whether to sort the eigenpairs in ascending eigenvalue order
     Returns:
-        l: array of eigenvalues, if sorted, by ascending algebraic order
+        l: array of eigenvalues
         v: corresponding eigenvectors as columns of matrix
     """
     l, v = nla.eigh(a)
@@ -27,30 +29,109 @@ def eigsys(a, sort=True):
 
 
 def eigvals(a, sort=True):
-    """ Find sorted eigenvalues of matrix
+    """ Find all eigenvalues of dense matrix
     Input:
         a: hermitian matrix
+        sort: whether to sort the eigenvalues in ascending order
     Returns:
-        l: array of eigenvalues, if sorted, by ascending algebraic order
+        l: array of eigenvalues
     """
     l = nla.eigvalsh(a)
     return np.sort(l) if sort else l
 
 
 def eigvecs(a, sort=True):
-    """ Find sorted eigenvectors of matrix
+    """ Find all eigenvectors of dense matrix
     Input:
         a: hermitian matrix
+        sort: whether to sort the eigenvectors in ascending eigenvalue order
     Returns:
-        v: eigenvectors as columns of matrix, if sorted, by ascending
-        eigenvalue order
+        v: eigenvectors as columns of matrix
     """
-    l, v = nla.eigh(a)
-    return qonvert(v[:, np.argsort(l)]) if sort else qonvert(v)
+    l, v = eigsys(a, sort=sort)
+    return v
+
+
+# iterative methods for partial eigendecompision #
+# ---------------------------------------------- #
+def seigsys(a, k=1, which='SA', ncv=None, return_vecs=True, **kwargs):
+    """ Returns a few eigenpairs from a possibly sparse hermitian operator
+    Inputs:
+        a: matrix, probably sparse, hermitian
+        k: number of eigenpairs to return
+        which: where in spectrum to take eigenvalues from (see scipy eigsh)
+        nvc: number of lanczos vectors, can use to optimise speed
+    Returns:
+        lk: array of eigenvalues
+        vk: matrix of eigenvectors as columns
+    """
+    n = a.shape[0]
+    sparse = sp.issparse(a)
+    if not sparse and n <= 500:  # Small dense matrices can use full decomp.
+        if return_vecs:
+            lk, vk = eigsys(a)
+            return lk[0:k], vk[:, 0:k]
+        else:
+            lk = eigvals(a)
+            return lk[0:k]
+    else:
+        ncv = calcncv(k, n, sparse) if ncv is None else ncv
+        if return_vecs:
+            lk, vk = spla.eigsh(a, k=k, which=which, ncv=ncv, **kwargs)
+            return lk, qonvert(vk)
+        else:
+            return spla.eigsh(a, k=k, which=which, ncv=ncv,
+                              return_eigenvectors=False, **kwargs)
+
+
+def seigvals(a, k=1, which='SA', ncv=None, **kwargs):
+    return seigsys(a, k=k, which=which, ncv=ncv, return_vecs=False, **kwargs)
+
+
+def seigvecs(a, k=1, which='SA', ncv=None, **kwargs):
+    l, v = seigsys(a, k, which, ncv, **kwargs)
+    return v
+
+
+def groundstate(ham):
+    """ Alias for finding lowest eigenvector only. """
+    return seigvecs(a, k=1, which='SA')
+
+
+def groundenergy(ham):
+    """ Alias for finding lowest eigenvalue only. """
+    return seigvals(ham, k=1, which='SA')
+
+
+# iterative methods for partial singular value decomposition #
+# ---------------------------------------------------------- #
+def svds(a, k=1, ncv=None, return_vecs=True, **kwargs):
+    """ Compute a number of singular value pairs """
+    n = a.shape[0]
+    sparse = sp.issparse(a)
+    if not sparse and n <= 500:  # small and dense --> use full decomposition
+        if return_vecs:
+            uk, sk, vtk = nla.svd(a)
+            return uk[:, 0:k], sk[0:k], vtk[0:k, :]
+        else:
+            return nla.svd(a, compute_uv=False)
+    else:
+        ncv = calcncv(k, n, sparse) if ncv is None else ncv
+        if return_vecs:
+            uk, sk, vtk = spla.svds(a, k=k, ncv=ncv, **kwargs)
+            return qonvert(uk), sk, qonvert(vtk)
+        else:
+            return spla.svds(a, k=k, ncv=ncv,
+                             return_singular_vectors=False, **kwargs)
+
+
+def norm(a):
+    """ Return the 2-norm of matrix, a, i.e. the largest singular value. """
+    return svds(a, k=1, return_vecs=False)[0]
 
 
 def calcncv(k, n, sparse):
-    """ Optimise number of lanczos vectors for...
+    """ Optimise number of lanczos vectors for iterative methods
     Args:
         k: number of target eigenvalues/singular values
         n: matrix size
@@ -64,75 +145,3 @@ def calcncv(k, n, sparse):
     else:
         ncv = max(10, n//2**5 - 1, k * 2 + 2)
     return ncv
-    pass
-
-
-def seigsys(a, k=1, which='SA', ncv=None, **kwargs):
-    """
-    Returns a few eigenpairs from a possibly sparse hermitian operator
-    Inputs:
-        a: matrix, probably sparse, hermitian
-        k: number of eigenpairs to return
-        which: where in spectrum to take eigenvalues from (see scipy eigsh)
-        nvc: number of lanczos vectors, can use to optimise speed
-    Returns:
-        lk: array of eigenvalues
-        vk: matrix of eigenvectors as columns
-    """
-    n = a.shape[0]
-    sparse = sp.issparse(a)
-    if not sparse and n <= 500:  # Small dense matrices can use full decomp.
-        lk, vk = eigsys(a)
-        return lk[0:k], vk[:, 0:k]
-    else:
-        ncv = calcncv(k, n, sparse) if ncv is None else ncv
-        lk, vk = spla.eigsh(a, k=k, which=which, ncv=ncv, **kwargs)
-        return lk, qonvert(vk)
-
-
-def seigvals(a, k=1, which='SA', ncv=None, **kwargs):
-    n = a.shape[0]
-    sparse = sp.issparse(a)
-    if not sparse and n <= 500:  # Small dense matrices can use full decomp.
-        lk = eigvals(a)
-        return lk[0:k]
-    else:
-        ncv = calcncv(k, n, sparse) if ncv is None else ncv
-        return spla.eigsh(a, k=k, which=which, ncv=ncv,
-                          return_eigenvectors=False, **kwargs)
-
-
-def seigvecs(a, k=1, which='SA', ncv=None, **kwargs):
-    l, v = seigsys(a, k, which, ncv, **kwargs)
-    return v
-
-
-def groundstate(ham):
-    """ Alias for finding lowest eigenvector only. """
-    return seigvecs(a)
-
-
-def groundenergy(ham):
-    """ Alias for finding lowest eigenvalue only. """
-    return seigvals(ham)
-
-
-def svds(a, k=1, ncv=None, **kwargs):
-    """
-    Compute a number of singular values
-    """
-    n = a.shape[0]
-    sparse = sp.issparse(a)
-    if not sparse and n <= 500:
-        u, s, vt = nla.svd(a)
-    else:
-        ncv = calcncv(k, n, sparse) if ncv is None else ncv
-        u, s, vt = spla.svds(a, k=k, ncv=ncv, **kwargs)
-    return qonvert(u), s, qonvert(vt)
-
-
-def norm(a):
-    """
-    Return the 2-norm of matrix, a, i.e. the largest singular value.
-    """
-    return svds(a, k=1, return_singular_vectors=False)[0]
