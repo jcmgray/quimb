e5b44cd7f301d51ade04053edda684a36f62c4ab
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-01-08 13:51:04 +0000

    move ham_heis into gen module, remove ham module

--- quijy/gen.py
+++ quijy/gen.py
@@ -4,7 +4,7 @@ Functions for generating quantum objects.
 
 import numpy as np
 import scipy.sparse as sp
-from quijy.core import qonvert, nrmlz, kron, kronpow
+from quijy.core import qonvert, nrmlz, kron, kronpow, eyepad, eye
 
 
 def basis_vec(dir, dim, sparse=False):
@@ -102,3 +102,33 @@ def singlet_pairs(n):
 
 def werner_state(p):
     return p * bell_state(3) * bell_state(3).H + (1 - p) * np.eye(4) / 4
+
+
+def ham_heis(n, jx=1, jy=1, jz=1, bz=0, periodic=False, sparse=False):
+    """ Constructs the heisenberg spin 1/2 hamiltonian
+    Input:
+        n: number of spins
+        jx, jy, jz: coupling constants, with convention that positive =
+        antiferromagnetic
+        bz: z-direction magnetic field
+        periodic: whether to couple the first and last spins
+        sparse: whether to return the hamiltonian in sparse form
+    Returns:
+        ham: hamiltonian as matrix
+    """
+    dims = [2] * n
+    sds = (jx * kron(sig('x', sparse=True), sig('x', sparse=True)) +
+           jy * kron(sig('y', sparse=True), sig('y', sparse=True)) +
+           jz * kron(sig('z', sparse=True), sig('z', sparse=True)) -
+           bz * kron(sig('z', sparse=True), eye(2)))
+    # Begin with last spin, not covered by loop
+    ham = eyepad(-bz * sig('z', sparse=True), dims, n - 1)
+    for i in range(n - 1):
+        ham = ham + eyepad(sds, dims[:-1], i)
+    if periodic:
+        ham = ham + eyepad(sig('x', sparse=True), dims, [0, n - 1])  \
+                  + eyepad(sig('y', sparse=True), dims, [0, n - 1])  \
+                  + eyepad(sig('z', sparse=True), dims, [0, n - 1])
+    if not sparse:
+        ham = ham.todense()  # always construct sparse though
+    return ham
