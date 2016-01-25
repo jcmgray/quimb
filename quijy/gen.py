b26122638cdf5b6bdbd49e60fc280ff7878bc95d
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-01-25 21:40:54 +0000

    Add qtype and sparse, multi fixes

--- quijy/gen.py
+++ quijy/gen.py
@@ -1,5 +1,6 @@
 """
 Functions for generating quantum objects.
+TODO: add sparse and qtype to all relevant functions.
 """
 
 import numpy as np
@@ -16,6 +17,7 @@ def basis_vec(dir, dim, sparse=False):
                              dtype=complex, shape=(dim, 1))
     else:
         x = np.zeros([dim, 1], dtype=complex)
+        x = qonvert(x)  # turn into matrix
         x[dir] = 1.0
     return x
 
@@ -34,29 +36,35 @@ def sig(xyz, sparse=False):
         return qonvert([[1, 0], [0, 1]], sparse=sparse)
 
 
-def bell_state(s):
+def bell_state(s, qtype='ket', sparse=False):
     """
     Generates one of the four bell-states;
     0: phi+, 1: phi-, 2: psi+, 3: psi- (singlet)
     """
-    if s in (3, 'psim'):
-        return qonvert([0, 1, -1, 0], 'ket') / 2.0**0.5
-    elif s in (0, 'phip'):
-        return qonvert([1, 0, 0, 1], 'ket') / 2.0**0.5
-    elif s in (1, 'phim'):
-        return qonvert([1, 0, 0, -1], 'ket') / 2.0**0.5
-    elif s in (2, 'psip'):
-        return qonvert([0, 1, 1, 0], 'ket') / 2.0**0.5
+    isqr2 = 2.0**-0.5
+    if s in (3, 'psi-'):
+        return qonvert([0, isqr2, -isqr2, 0], qtype=qtype, sparse=sparse)
+    elif s in (0, 'phi+'):
+        return qonvert([isqr2, 0, 0, isqr2], qtype=qtype, sparse=sparse)
+    elif s in (1, 'phi-'):
+        return qonvert([isqr2, 0, 0, -isqr2], qtype=qtype, sparse=sparse)
+    elif s in (2, 'psi+'):
+        return qonvert([0, isqr2, isqr2, 0], qtype=qtype, sparse=sparse)
 
 
-def singlet():
+def singlet(qtype='ket', sparse=False):
     """ Alias for one of bell-states """
-    return bell_state(3)
+    return bell_state(3, qtype=qtype, sparse=sparse)
+
+
+def triplets(sparse=False):
+    """ Equal mixture of the three triplet bell_states """
+    return eye(4, sparse=sparse) - singlet('p', sparse)
 
 
 def bloch_state(ax, ay, az, purify=False, sparse=False):
     if purify:
-        ax, ay, az = np.array([ax, ay, az])/np.sqrt(ax**2 + ay**2 + az**2)
+        ax, ay, az = np.array([ax, ay, az]) / (ax**2 + ay**2 + az**2)**0.5
     rho = 0.5 * (sig('i') + ax * sig('x') + ay * sig('y') + az * sig('z'))
     return rho if not sparse else qonvert(rho, sparse=sparse)
 
@@ -66,9 +74,8 @@ def random_psi(n):
     """
     Generates a wavefunction with random coefficients, normalised
     """
-    psi = 2.0 * np.matrix(np.random.rand(n, 1)) - 1
-    psi = psi + 2.0j * np.random.rand(n, 1) - 1.0j
-    return nrmlz(psi)
+    return qonvert(np.random.randn(n, 1) + 1.0j * np.random.randn(n, 1),
+                   nrmlz=True)
 
 
 def random_rho(n):
@@ -76,13 +83,12 @@ def random_rho(n):
     Generates a random density matrix of dimension n, no special properties
     other than being guarateed hermitian, positive, and trace 1.
     """
-    rho = 2.0 * np.matrix(np.random.rand(n, n)) - 1
-    rho = rho + 2.0j * np.matrix(np.random.rand(n, n)) - 1.0j
+    rho = qonvert(np.random.randn(n, n) + 1.0j * np.random.randn(n, n))
     rho = rho + rho.H
     return nrmlz(rho * rho)
 
 
-def random_product_state(n):
+def random_product_state(n, qtype=None):
     """
     Calculates the wavefunction of n many random pure qubits.
     """
@@ -93,8 +99,9 @@ def random_product_state(n):
             v = np.random.rand()
             phi = 2 * np.pi * u
             theta = np.arccos(2 * v - 1)
-            yield np.matrix([[np.cos(theta / 2.0)],
-                             [np.sin(theta / 2.0) * np.exp(1.0j * phi) ]])
+            yield qonvert([[np.cos(theta / 2.0)],
+                           [np.sin(theta / 2.0) * np.exp(1.0j * phi)]],
+                          qtype=qtype)
     return kron(*[x for x in calc_rand_pure_qubits(n)])
 
 
@@ -105,20 +112,21 @@ def neel_state(n):
 
 
 def singlet_pairs(n):
-    return kronpow(bell_state(3), (n / 2))
+    return kronpow(bell_state(3), (n // 2))
 
 
 def werner_state(p):
-    return p * bell_state(3) * bell_state(3).H + (1 - p) * np.eye(4) / 4
+    return p * bell_state(3) * bell_state(3).H + (1 - p) * eye(4) / 4
 
 
-def ghz_state(n):
-    return (basis_vec(0, 2**n) + basis_vec(2**n - 1, 2**n))/2.0**0.5
+def ghz_state(n, sparse=False):
+    return (basis_vec(0, 2**n, sparse=sparse) +
+            basis_vec(2**n - 1, 2**n, sparse=sparse))/2.0**0.5
 
 
 def ham_heis(n, jx=1, jy=1, jz=1, bz=0, periodic=False, sparse=False):
     """ Constructs the heisenberg spin 1/2 hamiltonian
-    Input:
+    Parameters:
         n: number of spins
         jx, jy, jz: coupling constants, with convention that positive =
         antiferromagnetic
@@ -144,3 +152,12 @@ def ham_heis(n, jx=1, jy=1, jz=1, bz=0, periodic=False, sparse=False):
     if not sparse:
         ham = ham.todense()  # always construct sparse though
     return ham
+
+
+def ham_j1j2(n, j1, j2, bz):
+    pass
+
+
+def ham_majumdar_ghosh(n):
+    """ Alias for ham-j1j2. """
+    pass
