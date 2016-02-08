9610ae05a1201d1e410912c9f96dd627c1493964
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-02-08 14:40:27 +0000

    rename many internal parameters

--- quijy/gen.py
+++ quijy/gen.py
@@ -2,11 +2,12 @@
 Functions for generating quantum objects.
 TODO: add sparse and qtype to all relevant functions.
 """
+# TODO: Graph states, cluster states, multidimensional
 
 import numpy as np
 import scipy.sparse as sp
 from itertools import product
-from quijy.core import (quijify, nrmlz, kron, kronpow, eyepad, eye, trx)
+from quijy.core import (qjf, nmlz, kron, kronpow, eyepad, eye, trx, eyeplace)
 
 
 def basis_vec(dir, dim, sparse=False):
@@ -18,65 +19,65 @@ def basis_vec(dir, dim, sparse=False):
                              dtype=complex, shape=(dim, 1))
     else:
         x = np.zeros([dim, 1], dtype=complex)
-        x = quijify(x)  # turn into matrix
+        x = qjf(x)  # turn into matrix
         x[dir] = 1.0
     return x
 
 
 def sig(xyz, sparse=False):
+def sig(s, **kwargs):
     """
-    Generates one of the three Pauli matrices, 0-X, 1-Y, 2-Z
+    Generates one of the three Pauli matrices, 0-I, 1-X, 2-Y, 3-Z
     """
-    if xyz in (1, 'x', 'X'):
-        return quijify([[0, 1], [1, 0]], sparse=sparse)
-    elif xyz in (2, 'y', 'Y'):
-        return quijify([[0, -1j], [1j, 0]], sparse=sparse)
-    elif xyz in (3, 'z', 'Z'):
-        return quijify([[1, 0], [0, -1]], sparse=sparse)
-    elif xyz in (0, 'i', 'I'):
-        return quijify([[1, 0], [0, 1]], sparse=sparse)
+    if s in (1, 'x', 'X'):
+        return qjf([[0, 1], [1, 0]], **kwargs)
+    elif s in (2, 'y', 'Y'):
+        return qjf([[0, -1j], [1j, 0]], **kwargs)
+    elif s in (3, 'z', 'Z'):
+        return qjf([[1, 0], [0, -1]], **kwargs)
+    elif s in (0, 'i', 'I'):
+        return qjf([[1, 0], [0, 1]], **kwargs)
 
 
-def bell_state(s, qtype='ket', sparse=False):
+def bell_state(s, **kwargs):
     """
     Generates one of the four bell-states;
     0: phi+, 1: phi-, 2: psi+, 3: psi- (singlet)
     """
     isqr2 = 2.0**-0.5
     if s in (3, 'psi-'):
-        return quijify([0, isqr2, -isqr2, 0], qtype=qtype, sparse=sparse)
+        return qjf([[0], [isqr2], [-isqr2], [0]], **kwargs)
     elif s in (0, 'phi+'):
-        return quijify([isqr2, 0, 0, isqr2], qtype=qtype, sparse=sparse)
+        return qjf([[isqr2], [0], [0], [isqr2]], **kwargs)
     elif s in (1, 'phi-'):
-        return quijify([isqr2, 0, 0, -isqr2], qtype=qtype, sparse=sparse)
+        return qjf([[isqr2], [0], [0], [-isqr2]], **kwargs)
     elif s in (2, 'psi+'):
-        return quijify([0, isqr2, isqr2, 0], qtype=qtype, sparse=sparse)
+        return qjf([[0], [isqr2], [isqr2], [0]], **kwargs)
 
 
-def singlet(qtype='ket', sparse=False):
+def singlet(**kwargs):
     """ Alias for one of bell-states """
-    return bell_state(3, qtype=qtype, sparse=sparse)
+    return bell_state(3, **kwargs)
 
 
-def triplets(sparse=False):
+def triplets(**kwargs):
     """ Equal mixture of the three triplet bell_states """
-    return eye(4, sparse=sparse) - singlet('p', sparse)
+    return eye(4, **kwargs) - singlet('p', **kwargs)
 
 
-def bloch_state(ax, ay, az, purify=False, sparse=False):
+def bloch_state(ax, ay, az, purify=False, **kwargs):
     if purify:
         ax, ay, az = np.array([ax, ay, az]) / (ax**2 + ay**2 + az**2)**0.5
-    rho = 0.5 * (sig('i') + ax * sig('x') + ay * sig('y') + az * sig('z'))
-    return rho if not sparse else quijify(rho, sparse=sparse)
+    return sum(0.5 * a * sig(s, **kwargs)
+               for a, s in zip((1, ax, ay, az), 'ixyz'))
 
 
-# functions
 def rand_ket(n):
     """
     Generates a wavefunction with random coefficients, normalised
     """
-    return quijify(np.random.randn(n, 1) + 1.0j * np.random.randn(n, 1),
-                   nrmlzd=True)
+    return qjf(np.random.randn(n, 1) + 1.0j * np.random.randn(n, 1),
+               normalized=True)
 
 
 def rand_rho(n):
@@ -84,9 +85,9 @@ def rand_rho(n):
     Generates a random density matrix of dimension n, no special properties
     other than being guarateed hermitian, positive, and trace 1.
     """
-    rho = quijify(np.random.randn(n, n) + 1.0j * np.random.randn(n, n))
+    rho = qjf(np.random.randn(n, n) + 1.0j * np.random.randn(n, n))
     rho = rho + rho.H
-    return nrmlz(rho * rho)
+    return nmlz(rho * rho)
 
 
 def rand_mix(n):
@@ -111,9 +112,9 @@ def rand_product_state(n, qtype=None):
             v = np.random.rand()
             phi = 2 * np.pi * u
             theta = np.arccos(2 * v - 1)
-            yield quijify([[np.cos(theta / 2.0)],
-                           [np.sin(theta / 2.0) * np.exp(1.0j * phi)]],
-                          qtype=qtype)
+            yield qjf([[np.cos(theta / 2.0)],
+                       [np.sin(theta / 2.0) * np.exp(1.0j * phi)]],
+                      qtype=qtype)
     return kron(*[x for x in calc_rand_pure_qubits(n)])
 
 
@@ -170,7 +171,6 @@ def ham_j1j2(n, j1=1.0, j2=0.5, bz=0.0, cyclic=False, sparse=False):
     dims = [2] * n
     coosj1 = np.array([(i, i+1) for i in range(n)])
     coosj2 = np.array([(i, i+2) for i in range(n)])
-
     s = [sig(i, sparse=True) for i in 'xyz']
     if cyclic:
         coosj1, coosj2 = coosj1 % n, coosj2 % n
@@ -180,20 +180,22 @@ def ham_j1j2(n, j1=1.0, j2=0.5, bz=0.0, cyclic=False, sparse=False):
 
     def gen_j1():
         for op, coo in product(s, coosj1):
-            yield eyepad(op, dims, sorted(coo), sparse=True)
+            yield eyeplace([op], dims, coo)
 
     def gen_j2():
         for op, coo in product(s, coosj2):
-            yield eyepad(op, dims, sorted(coo), sparse=True)
+            yield eyeplace([op], dims, coo)
 
     def gen_bz():
         for i in range(n):
-            yield eyepad(bz * s[2], dims, i)
+            yield eyeplace([s[2]], dims, i)
 
-    ham = j1 * sum(gen_j1()) + j2 * sum(gen_j2()) + bz * sum(gen_bz())
+    ham = j1 * sum(gen_j1()) + j2 * sum(gen_j2()) * bz * sum(gen_bz())
     return ham if sparse else ham.todense()
 
 
-def ham_majumdar_ghosh(n):
+def ham_majumdar_ghosh(n, j1=1.0, j2=0.5, **kwargs):
     """ Alias for ham-j1j2. """
-    pass
+    return ham_j1j2(n, j1=j1, j2=j2, **kwargs)
+
+
