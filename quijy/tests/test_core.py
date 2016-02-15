8265885c430ecfb6de0c25d7fcf632a754cd016b
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-02-15 12:17:37 +0000

    update test_core with shape checks, quick notation

--- quijy/tests/test_core.py
+++ quijy/tests/test_core.py
@@ -1,26 +1,27 @@
 import scipy.sparse as sp
 import numpy as np
 from numpy.testing import assert_allclose
-from nose.tools import assert_almost_equal, assert_equal
+from nose.tools import assert_almost_equal, eq_, ok_
 from quijy.core import *
 
 
+# Quijify
 def test_quijify_vector_create():
     x = [1, 2, 3j]
     p = quijify(x, qtype='ket')
-    assert_equal(type(p), np.matrix)
-    assert_equal(p.dtype, np.complex)
-    assert_equal(p.shape, (3, 1))
+    eq_(type(p), np.matrix)
+    eq_(p.dtype, np.complex)
+    eq_(p.shape, (3, 1))
     p = quijify(x, qtype='bra')
-    assert_equal(p.shape, (1, 3))
+    eq_(p.shape, (1, 3))
     assert_almost_equal(p[0,2], -3.0j)
 
 def test_quijify_dop_create():
     x = np.random.randn(3, 3)
     p = quijify(x, qtype='dop')
-    assert_equal(type(p), np.matrix)
-    assert_equal(p.dtype, np.complex)
-    assert_equal(p.shape, (3, 3))
+    eq_(type(p), np.matrix)
+    eq_(p.dtype, np.complex)
+    eq_(p.shape, (3, 3))
 
 def test_quijify_convert_vector_to_dop():
     x = [1, 2, 3j]
@@ -33,9 +34,9 @@ def test_quijify_convert_vector_to_dop():
 def test_quijify_chopped():
     x = [9e-16, 1]
     p = quijify(x, 'k', chopped=False)
-    assert(p[0, 0] != 0.0)
+    ok_(p[0, 0] != 0.0)
     p = quijify(x, 'k', chopped=True)
-    assert_equal(p[0, 0], 0.0)
+    eq_(p[0, 0], 0.0)
 
 
 def test_quijify_normalized():
@@ -50,19 +51,41 @@ def test_quijify_normalized():
 def test_quijify_sparse_create():
     x = [[1, 0], [3, 0]]
     p = quijify(x, 'dop', sparse=False)
-    assert_equal(type(p), np.matrix)
+    eq_(type(p), np.matrix)
     p = quijify(x, 'dop', sparse=True)
-    assert_equal(type(p), sp.csr_matrix)
-    assert_equal(p.dtype, np.complex)
-    assert_equal(p.nnz, 2)
+    eq_(type(p), sp.csr_matrix)
+    eq_(p.dtype, np.complex)
+    eq_(p.nnz, 2)
 
 
 def test_quijify_sparse_convert_to_dop():
     x = [1, 0, 9e-16, 0, 3j]
     p = quijify(x, 'ket', sparse=True)
     q = quijify(p, 'dop', sparse=True)
-    assert_equal(q.shape, (5, 5))
-    assert_equal(q.nnz, 9)
+    eq_(q.shape, (5, 5))
+    eq_(q.nnz, 9)
     assert_almost_equal(q[4, 4], 9.)
     q = quijify(p, 'dop', sparse=True, normalized=True)
     assert_almost_equal(tr(q), 1.)
+
+
+# Shape checks
+def test_ket():
+    x = qjf([[1],[0]])
+    ok_(isket(x))
+    ok_(not isbra(x))
+    ok_(not isop(x))
+
+
+def test_bra():
+    x = qjf([[1 , 0]])
+    ok_(not isket(x))
+    ok_(isbra(x))
+    ok_(not isop(x))
+
+
+def test_op():
+    x = qjf([[1 , 0]])
+    ok_(not isket(x))
+    ok_(isbra(x))
+    ok_(not isop(x))
