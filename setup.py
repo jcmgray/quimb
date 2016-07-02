75410ce54171fb1a26fe5f1c8856349ac86dd4f6
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-07-02 14:36:30 +0100

    initial travis.ci commit

--- setup.py
+++ setup.py
@@ -5,14 +5,12 @@ setup(name='quimb',
       author='Johnnie Gray',
       license='MIT',
       packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-      install_requires=[
-          'numpy>=1.10',
-          'scipy>=0.15',
-          'numba>=0.22',
-          'numexpr>=2.3',
-          ],
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.5',
-          ])
+      install_requires=['numpy>=1.10',
+                        'scipy>=0.15',
+                        'numba>=0.22',
+                        'numexpr>=2.3',
+                        ],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.5',
+                   ])
