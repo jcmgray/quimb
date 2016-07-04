82e8ba8f2813d48112b43ace39d8da0674959621
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-07-04 10:41:18 +0100

    fix travis slepc #4 remove mpi

--- deps/travis-install-slepc4py.sh
+++ deps/travis-install-slepc4py.sh
@@ -6,63 +6,70 @@
 
 set -e
 
-mkdir $HOME/petsc_and_slepc
-cd $HOME/petsc_and_slepc
+export INSTALL_DIR=$HOME/petsc_and_slepc
+
+mkdir $INSTALL_DIR
+cd $INSTALL_DIR
+
 # Download required repositories
 git clone --depth 1 https://github.com/xianyi/OpenBLAS.git
 git clone --depth 1 https://bitbucket.org/petsc/petsc.git
 git clone --depth 1 https://bitbucket.org/slepc/slepc.git
-git clone --depth 1 https://bitbucket.org/mpi4py/mpi4py.git
+# git clone --depth 1 https://bitbucket.org/mpi4py/mpi4py.git
 git clone --depth 1 https://bitbucket.org/petsc/petsc4py.git
 git clone --depth 1 https://bitbucket.org/slepc/slepc4py.git
 
-# if [ ! -d "$HOME/petsc_and_slepc" ]; then
+# if [ ! -d "$INSTALL_DIR" ]; then
 # else
 #   echo 'Using cached petsc_and_slepc directory.';
 # fi
 
 # Build Openblas
-cd $HOME/petsc_and_slepc/OpenBLAS
+cd $INSTALL_DIR/OpenBLAS
 git pull
-make
+make -s
 
 # Build PETSc
-export PETSC_DIR=$HOME/petsc_and_slepc/petsc
+export PETSC_DIR=$INSTALL_DIR/petsc
 export PETSC_ARCH=arch-linux2-c-release-openblas
 cd $PETSC_DIR
 git pull
 python2 ./configure \
-  --with-blas-lapack-lib=$HOME/petsc_and_slepc/OpenBLAS/libopenblas.a \
-  --with-scalar-type=complex \
-  --download-mumps \
-  --download-scalapack \
-  --download-parmetis \
-  --download-metis --download-ptscotch \
-  --with-debugging=0 \
-  COPTFLAGS='-O3 -march=native -mtune=native' \
-  CXXOPTFLAGS='-O3 -march=native -mtune=native' \
+  --with-mpi=0  \  # THIS IS FOR TRAVIS ONLY
+  --with-blas-lapack-lib=$INSTALL_DIR/OpenBLAS/libopenblas.a \
+  --with-scalar-type=complex  \
+  --download-mumps  \
+  --download-scalapack  \
+  --download-parmetis  \
+  --download-metis --download-ptscotch  \
+  --with-debugging=0  \
+  COPTFLAGS='-O3 -march=native -mtune=native'  \
+  CXXOPTFLAGS='-O3 -march=native -mtune=native'  \
   FOPTFLAGS='-O3 -march=native -mtune=native'
-make all
+make -s all
 make test
 make streams
 
 # Build SLEPc
-export SLEPC_DIR=$HOME/petsc_and_slepc/slepc
+export SLEPC_DIR=$INSTALL_DIR/slepc
 cd $SLEPC_DIR
 git pull
 python2 ./configure
-make
+make -s
 make test
 
-# Install python packages
-cd $HOME/petsc_and_slepc/mpi4py
+# ----------------------- #
+# Install python packages #
+# ----------------------- #
+
+# cd $INSTALL_DIR/mpi4py
+# git pull
+# pip install --no-deps .
+
+cd $INSTALL_DIR/petsc4py
 git pull
 pip install --no-deps .
 
-cd $HOME/petsc_and_slepc/petsc4py
-git pull
-pip install --no-deps .
-
-cd $HOME/petsc_and_slepc/slepc4py
+cd $INSTALL_DIR/slepc4py
 git pull
 pip install --no-deps .
