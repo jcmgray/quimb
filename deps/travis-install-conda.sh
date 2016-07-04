1ec28964cc5e6360710d4f372b15ba166707a4ab
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-07-04 11:56:34 +0100

    fix conda test env

--- deps/travis-install-conda.sh
+++ deps/travis-install-conda.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-export CONDA_PATH=$HOME/conda
-
-if [ ! -d "$CONDA_PATH" ]; then
+if [ ! -d "$HOME/conda" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-  bash miniconda.sh -b -p $CONDA_PATH
-  export PATH="$CONDA_PATH/bin:$PATH"
+  bash miniconda.sh -b -p $HOME/conda
+  export PATH="$HOME/conda/bin:$PATH"
   hash -r
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
@@ -13,11 +11,11 @@ if [ ! -d "$CONDA_PATH" ]; then
   conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy numba numexpr coverage
   pip install pytest-cov coveralls
 else
-  export PATH="$CONDA_PATH/bin:$PATH"
+  export PATH="$HOME/conda/bin:$PATH"
   conda config --set always_yes yes --set changeps1 no
-  conda update -q conda
+  conda update conda
   conda info -a
   source activate test-environment
-  conda update -q --all
+  conda install numpy scipy numba numexpr coverage
   pip install -U pytest-cov coveralls
 fi
