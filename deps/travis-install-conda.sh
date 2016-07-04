977ecaba92e788f084a8b2d27f608586e0cea33c
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-07-04 12:40:53 +0100

    change cache detection logic

--- deps/travis-install-conda.sh
+++ deps/travis-install-conda.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/conda" ]; then
+if [ ! -d "$HOME/conda/bin" ]; then
+  rm -rf $HOME/conda
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/conda
   export PATH="$HOME/conda/bin:$PATH"
