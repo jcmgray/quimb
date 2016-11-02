868b94634951e34ccb77f1299724fff1cc1b9a23
jcmgray <john.gray.14@ucl.ac.uk>
2016-11-02 11:48:47 +0000

    fix cytoolz version

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(name='quimb',
-      version='0.2.1',
-      author='Johnnie Gray',
-      license='MIT',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-      install_requires=['numpy>=1.10',
-                        'scipy>=0.15',
-                        'numba>=0.22',
-                        'numexpr>=2.3',
-                        'psutil>=4.3.1',
-                        'cytoolz>=8.0.0',
-                        ],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python :: 3.5',
-                   ])
+setup(
+    name='quimb',
+    version='0.2.1',
+    author='Johnnie Gray',
+    license='MIT',
+    packages=find_packages(exclude=['deps', 'tests*']),
+    install_requires=[
+        'numpy>=1.10',
+        'scipy>=0.15',
+        'numba>=0.22',
+        'numexpr>=2.3',
+        'psutil>=4.3.1',
+        'cytoolz>=0.8.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
