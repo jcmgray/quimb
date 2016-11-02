807397c7e6b99d501428e032780c4a02e20a76be
jcmgray <john.gray.14@ucl.ac.uk>
2016-11-02 10:32:02 +0000

    temp fix for conda issue
    try travis fix #3
    
    
    conda build changes
    
    
    debugging prints for conda install
    
    
    try removing python travis specification

--- deps/travis-install-conda.sh
+++ deps/travis-install-conda.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -6,16 +7,19 @@ if [ ! -d "$HOME/conda/bin" ]; then
   if [ -d "$HOME/conda" ]; then
     rm -rf $HOME/conda
   fi
+  echo "Creating new conda installation."
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/conda
   export PATH="$HOME/conda/bin:$PATH"
   hash -r
   conda config --set always_yes yes --set changeps1 no
+  conda install pyyaml
   conda update -q conda
   conda info -a
   conda env create --file $DIR/requirements-py35.yml
   source activate test-environment
 else
+  echo "Using cached conda installation."
   export PATH="$HOME/conda/bin:$PATH"
   hash -r
   conda config --set always_yes yes --set changeps1 no
