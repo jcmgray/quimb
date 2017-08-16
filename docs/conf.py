6a80123d0d3e3d1a67ffa53f3ad40b7deb8e761f
Johnnie Gray <john.gray.14@ucl.ac.uk>
2017-08-16 17:56:00 +0100

    try bootstrap theme

--- docs/conf.py
+++ docs/conf.py
@@ -130,11 +130,16 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 # html_theme = 'alabaster'
 # html_theme = 'flask'
 # html_theme = 'bootstrap'
-# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+import sphinx_bootstrap_theme
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_options = {
+  'bootswatch_theme': "journal"
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
