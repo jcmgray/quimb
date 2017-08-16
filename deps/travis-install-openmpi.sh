1ae760354bea2ae8831f9dd918ac37d30331c419
Johnnie Gray <john.gray.14@ucl.ac.uk>
2017-08-16 17:45:24 +0100

    travis: re-enable mpi4py install

--- deps/travis-install-openmpi.sh
+++ deps/travis-install-openmpi.sh
@@ -4,7 +4,7 @@ set -ex
 export LOCAL=${LOCAL:-"$HOME/local"}
 
 # Check for pre existing mpi installation
-if [ -e $LOCAL/lib/libmpi.so ]; then
+if [ pip list | grep mpi4py &>/dev/null ]; then
     echo "mpi already installed"
     exit 0
 fi
@@ -18,15 +18,15 @@ MPI4PY_REPO=${MPI4PY_REPO:-"https://bitbucket.org/mpi4py/mpi4py.git"}
 mkdir -p $LOCAL
 mkdir -p $SRC_DIR
 
-# download and extract openmpi
-wget "https://www.open-mpi.org/software/ompi/v${OPENMPI_VER:8:3}/downloads/$OPENMPI_VER.tar.gz" -P $SRC_DIR
-tar xzf "$SRC_DIR/$OPENMPI_VER.tar.gz" -C $SRC_DIR
-cd "$SRC_DIR/$OPENMPI_VER"
+# # download and extract openmpi
+# wget "https://www.open-mpi.org/software/ompi/v${OPENMPI_VER:8:3}/downloads/$OPENMPI_VER.tar.gz" -P $SRC_DIR
+# tar xzf "$SRC_DIR/$OPENMPI_VER.tar.gz" -C $SRC_DIR
+# cd "$SRC_DIR/$OPENMPI_VER"
 
-# compile and install
-./configure --prefix=$LOCAL COPTFLAGS='-O0' CXXOPTFLAGS='-O0' FOPTFLAGS='-O0'
-make
-make install
+# # compile and install
+# ./configure --prefix=$LOCAL COPTFLAGS='-O0' CXXOPTFLAGS='-O0' FOPTFLAGS='-O0'
+# make
+# make install
 
 # install python package
 cd $SRC_DIR
