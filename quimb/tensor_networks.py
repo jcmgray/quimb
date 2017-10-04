47c4c9d8e322de4f2e25385c8cd08906fc154094
Johnnie Gray <john.gray.14@ucl.ac.uk>
2017-10-04 18:17:01 +0100

    first commit of tensor network stuff

---
+++ quimb/tensor_networks.py
@@ -0,0 +1,232 @@
+import os
+import functools
+from functools import reduce
+import operator
+from operator import or_
+
+from cytoolz import concat, frequencies, interleave
+import numpy as np
+
+from .linalg.approx_spectral import contract
+
+
+# --------------------------------------------------------------------------- #
+#                                Tensor Class                                 #
+# --------------------------------------------------------------------------- #
+
+class Tensor(object):
+    """
+    """
+
+    def __init__(self, array, inds, tags=None):
+        self.array = np.asarray(array)
+        self.inds = tuple(inds)
+        self.tags = (set() if tags is None else
+                     {tags} if isinstance(tags, str) else
+                     set(tags))
+
+    def conj(self):
+        return Tensor(self.array.conj(), self.inds, self.tags)
+
+    @property
+    def H(self):
+        """
+        """
+        return self.conj()
+
+    @property
+    def shape(self):
+        return self.array.shape
+
+    @property
+    def size(self):
+        return self.array.size
+
+    def __and__(self, other):
+        """Combine with another ``Tensor`` or ``TensorNetwork`` into a new
+        ``TensorNetwork``.
+        """
+        if isinstance(other, Tensor):
+            return TensorNetwork(self, other)
+        elif isinstance(other, TensorNetwork):
+            return TensorNetwork(self, *other.tensors)
+        else:
+            raise TypeError("Cannot combine with object of "
+                            "type {}".format(type(other)))
+
+    def __matmul__(self, other):
+        """Explicitly contract with another tensor.
+        """
+        return tensor_contract(self, other)
+
+    def __repr__(self):
+        return "Tensor(shape={}, inds={}, tags={})".format(
+            self.array.shape,
+            self.inds,
+            self.tags)
+
+
+# ------------------------- Add ufunc like methods -------------------------- #
+
+def _make_promote_array_func(op, meth_name):
+
+    @functools.wraps(getattr(np.ndarray, meth_name))
+    def _promote_array_func(self, other):
+        """Use standard array func, but make sure Tensor inds match.
+        """
+        if isinstance(other, Tensor):
+            if self.inds != other.inds:
+                raise ValueError(
+                    "The indicies of these two tensors do not "
+                    "match: {} != {}".format(self.inds, other.inds))
+            return Tensor(
+                array=op(self.array, other.array), inds=self.inds,
+                tags=self.tags | other.tags)
+        return Tensor(array=op(self.array, other),
+                      inds=self.inds, tags=self.tags)
+
+    return _promote_array_func
+
+
+for meth_name, op in [('__add__', operator.__add__),
+                      ('__sub__', operator.__sub__),
+                      ('__mul__', operator.__mul__),
+                      ('__pow__', operator.__pow__),
+                      ('__truediv__', operator.__truediv__)]:
+    setattr(Tensor, meth_name, _make_promote_array_func(op, meth_name))
+
+
+def _make_rhand_array_promote_func(op, meth_name):
+
+    @functools.wraps(getattr(np.ndarray, meth_name))
+    def _rhand_array_promote_func(self, other):
+        """Right hand operations -- no need to check ind equality first.
+        """
+        return Tensor(array=op(other, self.array),
+                      inds=self.inds, tags=self.tags)
+
+    return _rhand_array_promote_func
+
+
+for meth_name, op in [('__radd__', operator.__add__),
+                      ('__rsub__', operator.__sub__),
+                      ('__rmul__', operator.__mul__),
+                      ('__rpow__', operator.__pow__),
+                      ('__rtruediv__', operator.__truediv__)]:
+    setattr(Tensor, meth_name, _make_rhand_array_promote_func(op, meth_name))
+
+
+# --------------------------------------------------------------------------- #
+#                                Tensor Funcs                                 #
+# --------------------------------------------------------------------------- #
+
+def gen_output_inds(inds):
+    """Generate the output indices from the set ``inds``.
+    """
+    freqs = frequencies(concat(inds))
+
+    for ind, freq in freqs.items():
+        if freq > 2:
+            raise ValueError("The index {} appears more "
+                             "than twice!".format(ind))
+        elif freq == 1:
+            yield ind
+
+
+def tensor_contract(*tensors, memory_limit=-1, optimize=True):
+    """Efficiently contract some tensors.
+    """
+    i_inds = [t.inds for t in tensors]
+    o_inds = sorted(gen_output_inds(i_inds))
+
+    o_array = contract(*interleave(((t.array for t in tensors), i_inds)),
+                       o_inds, memory_limit=memory_limit, optimize=optimize)
+
+    if not o_inds:
+        return o_array
+
+    # unison of all tags
+    o_tags = reduce(or_, (t.tags for t in tensors))
+
+    return Tensor(array=o_array, inds=o_inds, tags=o_tags)
+
+
+# --------------------------------------------------------------------------- #
+#                            Tensor Network Class                             #
+# --------------------------------------------------------------------------- #
+
+class TensorNetwork(object):
+    """
+
+    Members
+    -------
+    tensors : sequence of Tensor
+        The tensors in this network.
+    """
+
+    def __init__(self, *tensors):
+        self.tensors = []
+        for t in tensors:
+            if isinstance(t, Tensor):
+                self.tensors.append(t)
+            elif isinstance(t, TensorNetwork):
+                self.tensors += t.tensors
+
+    def contract(self, tags=None):
+        """
+        """
+        leave, lose = self.filt(tags)
+
+        if leave:
+            return TensorNetwork(*leave) & tensor_contract(*lose)
+
+        return tensor_contract(*lose)
+
+    def filt(self, tags):
+        """
+        """
+        # contract all
+        if tags is None:
+            return [], self.tensors
+
+        if isinstance(tags, str):
+            tags = {tags}
+
+        leave, lose = [], []
+        for t in self.tensors:
+            if any(tag in t.tags for tag in tags):
+                lose.append(t)
+            else:
+                leave.append(t)
+
+        return leave, lose
+
+    def conj(self):
+        return TensorNetwork(*[t.conj() for t in self.tensors])
+
+    @property
+    def H(self):
+        return self.conj()
+
+    def __rshift__(self, tags=None):
+        return self.contract(tags)
+
+    def __and__(self, other):
+        """Combine this tensor network with more tensors, without contracting.
+        """
+        if isinstance(other, Tensor):
+            return TensorNetwork(*self.tensors, other)
+
+        return TensorNetwork(*self.tensors, *other.tensors)
+
+    def __repr__(self):
+        return "TensorNetwork({})".format(
+            repr(self.tensors))
+
+    def __str__(self):
+        return "TensorNetwork([{}{}{}{}])".format(
+            os.linesep,
+            "".join(["    " + repr(t) + os.linesep
+                     for t in self.tensors[:-1]]),
+            "    " + repr(self.tensors[-1]),
+            os.linesep)
---
+++ tests/test_tensor_networks.py
@@ -0,0 +1,154 @@
+import pytest
+import operator
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+from quimb.tensor_networks import (
+    Tensor,
+    tensor_contract,
+    TensorNetwork,
+)
+
+
+class TestBasicTensorOperations:
+
+    def test_tensor_construct(self):
+        x = np.random.randn(2, 3, 4)
+        a = Tensor(x, inds=[0, 1, 2], tags='blue')
+        assert_allclose(a.H.array, x.conj())
+        assert a.size == 24
+
+    def test_arithmetic_scalar(self):
+        x = np.random.randn(2, 3, 4)
+        a = Tensor(x, inds=[0, 1, 2], tags='blue')
+        assert_allclose((a + 2).array, x + 2)
+        assert_allclose((a - 3).array, x - 3)
+        assert_allclose((a * 4).array, x * 4)
+        assert_allclose((a / 5).array, x / 5)
+        assert_allclose((a ** 2).array, x ** 2)
+        assert_allclose((2 + a).array, 2 + x)
+        assert_allclose((3 - a).array, 3 - x)
+        assert_allclose((4 * a).array, 4 * x)
+        assert_allclose((5 / a).array, 5 / x)
+        assert_allclose((5 ** a).array, 5 ** x)
+
+    @pytest.mark.parametrize("op", [operator.__add__,
+                                    operator.__sub__,
+                                    operator.__mul__,
+                                    operator.__pow__,
+                                    operator.__truediv__])
+    @pytest.mark.parametrize("mismatch", (True, False))
+    def test_tensor_tensor_arithmetic(self, op, mismatch):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2], tags='blue')
+        b = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2], tags='red')
+        if mismatch:
+            b.inds = (0, 1, 3)
+            with pytest.raises(ValueError):
+                op(a, b)
+        else:
+            c = op(a, b)
+            assert_allclose(c.array, op(a.array, b.array))
+
+    def test_contract_some(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2])
+        b = Tensor(np.random.randn(3, 4, 5), inds=[1, 2, 3])
+        c = a @ b
+
+        assert isinstance(c, Tensor)
+        assert c.shape == (2, 5)
+        assert c.inds == (0, 3)
+
+    def test_contract_all(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2])
+        b = Tensor(np.random.randn(3, 4, 2), inds=[1, 2, 0])
+        c = a @ b
+        assert isinstance(c, float)
+        assert not isinstance(c, Tensor)
+
+    def test_contract_None(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2])
+        b = Tensor(np.random.randn(3, 4, 5), inds=[3, 4, 5])
+        c = a @ b
+        assert c.shape == (2, 3, 4, 3, 4, 5)
+        assert c.inds == (0, 1, 2, 3, 4, 5)
+
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2])
+        b = Tensor(np.random.randn(3, 4, 5), inds=[5, 4, 3])
+        c = a @ b
+        assert c.shape == (2, 3, 4, 5, 4, 3)
+        assert c.inds == (0, 1, 2, 3, 4, 5)
+
+    def test_raise_on_triple_inds(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2])
+        b = Tensor(np.random.randn(3, 4, 5), inds=[1, 1, 2])
+        with pytest.raises(ValueError):
+            a @ b
+
+    def test_multi_contract(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2],
+                   tags='red')
+        b = Tensor(np.random.randn(3, 4, 5), inds=[1, 2, 3],
+                   tags='blue')
+        c = Tensor(np.random.randn(5, 2, 6), inds=[3, 0, 4],
+                   tags='blue')
+        d = tensor_contract(a, b, c)
+        assert isinstance(d, Tensor)
+        assert d.shape == (6,)
+        assert d.inds == (4,)
+        assert d.tags == {'red', 'blue'}
+
+
+class TestTensorNetworkBasic:
+    def test_combining_tensors(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2],
+                   tags='red')
+        b = Tensor(np.random.randn(3, 4, 5), inds=[1, 2, 3],
+                   tags='blue')
+        c = Tensor(np.random.randn(5, 2, 6), inds=[3, 0, 4],
+                   tags='blue')
+
+        with pytest.raises(TypeError):
+            a & np.array([0, 0])
+
+        abc1 = (a & b & c).H.contract()
+        abc2 = (a & (b & c)).H.contract()
+        abc3 = (TensorNetwork(a, b, c)).H.contract()
+        abc4 = (TensorNetwork(a, TensorNetwork(b, c))).H.contract()
+        abc5 = (TensorNetwork(a) & TensorNetwork(b, c)).H.contract()
+
+        assert_allclose(abc1.array, abc2.array)
+        assert_allclose(abc1.array, abc3.array)
+        assert_allclose(abc1.array, abc4.array)
+        assert_allclose(abc1.array, abc5.array)
+
+    def test_contracting_tensors(self):
+        a = Tensor(np.random.randn(2, 3, 4), inds=[0, 1, 2],
+                   tags='red')
+        b = Tensor(np.random.randn(3, 4, 5), inds=[1, 2, 3],
+                   tags='blue')
+        c = Tensor(np.random.randn(5, 2, 6), inds=[3, 0, 4],
+                   tags='blue')
+
+        a_b_c = a & b & c
+        print(a_b_c)
+
+        assert isinstance(a_b_c, TensorNetwork)
+        a_bc = a_b_c >> 'blue'
+        assert isinstance(a_bc, TensorNetwork)
+        assert len(a_bc.tensors) == 2
+        abc = a_bc >> ['red', 'blue']
+        assert isinstance(abc, Tensor)
+        assert_allclose(abc.array, a_b_c.contract().array)
+
+    def test_entanglement_of_mps_state(self):
+
+        mps = TensorNetwork(
+            Tensor(np.random.randn(2, 8), [0, 1]),
+            *[Tensor(np.random.randn(8, 2, 8), [2 * i - 1, 2 * i, 2 * i + 1])
+              for i in range(1, 9)],
+            Tensor(np.random.randn(8, 2), [17, 18])
+        )
+
+        psi = mps.contract()
+        assert psi.shape == (2,) * 10
