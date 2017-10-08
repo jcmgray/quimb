10a803e8ab785751fe3869f98959a9fd07fbe20f
jcmgray <john.gray.14@ucl.ac.uk>
2017-10-09 00:50:18 +0100

    travis: explicitly install openmpi

--- deps/travis-install-openmpi.sh
+++ deps/travis-install-openmpi.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
 set -ex
 
-# install directory for mpi4py and maybe mpi itself
-export INSTALL_DIR=$HOME/mpi_stuff
-
 # Check for pre existing mpi installation
 if [ pip list | grep mpi4py &>/dev/null ]; then
-    echo "mpi already installed"
+    echo "mpi4py already installed"
     exit 0
 fi
 
-# make folders
-mkdir -p $INSTALL_DIR
+# install directory for mpi4py and maybe mpi itself
+export BUILD_DIR="$HOME/mpi_stuff"
+mkdir -p $BUILD_DIR
+export INSTALL_DIR="$BUILD_DIR/openmpi_install"
+
 
 # # download and extract openmpi
-# OPENMPI_VER=${OPENMPI_VER:-"openmpi-2.1.1"}
-# wget "https://www.open-mpi.org/software/ompi/v${OPENMPI_VER:8:3}/downloads/$OPENMPI_VER.tar.gz" -P $INSTALL_DIR
-# tar xzf "$INSTALL_DIR/$OPENMPI_VER.tar.gz" -C $INSTALL_DIR
-# cd "$INSTALL_DIR/$OPENMPI_VER"
+OPENMPI_VER=${OPENMPI_VER:-"openmpi-1.10.7"}
+if [ ${OPENMPI_VER:11:1} = '.' ]; then
+  DOWNLOAD_PREFIX=v${OPENMPI_VER:8:3}
+else
+  DOWNLOAD_PREFIX=v${OPENMPI_VER:8:4}
+fi
 
-# # compile and install
-# ./configure --prefix=$LOCAL COPTFLAGS='-O0' CXXOPTFLAGS='-O0' FOPTFLAGS='-O0'
-# make
-# make install
+TAR_REMOTE="https://www.open-mpi.org/software/ompi/$DOWNLOAD_PREFIX/downloads/$OPENMPI_VER.tar.gz"
+wget $TAR_REMOTE -P $BUILD_DIR
+tar xzf "$BUILD_DIR/$OPENMPI_VER.tar.gz" -C $BUILD_DIR
+cd "$BUILD_DIR/$OPENMPI_VER"
+
+# compile and install
+./configure \
+    COPTFLAGS='-O0' \
+    CXXOPTFLAGS='-O0' \
+    FOPTFLAGS='-O0' \
+    --prefix=$INSTALL_DIR \
+    --disable-dlopen
+make
+make install
 
 # install python package
 MPI4PY_REPO=${MPI4PY_REPO:-"https://bitbucket.org/mpi4py/mpi4py.git"}
-cd $INSTALL_DIR
+cd $BUILD_DIR
 git clone $MPI4PY_REPO
-cd $INSTALL_DIR/mpi4py
-python setup.py build
-python setup.py install
+cd $BUILD_DIR/mpi4py
+pip install --no-deps -U .
