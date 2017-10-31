1ff8463beaacb4375c571d71cca9a96a381b5fa3
Johnnie Gray <john.gray.14@ucl.ac.uk>
2017-10-31 12:12:18 +0000

    add MPOSpinHam class and ising, XY as examples

--- quimb/tensor/tensor_gen.py
+++ quimb/tensor/tensor_gen.py
@@ -1,7 +1,5 @@
 """Generate specific tensor network states and operators.
 """
-import functools
-
 import numpy as np
 
 from ..accel import make_immutable
@@ -61,49 +59,129 @@ def MPS_rand(n, bond_dim, phys_dim=2,
     return rmps
 
 
-@functools.lru_cache(128)
-def mpo_site_ham_heis(j=1.0, bz=0.0):
-    """Single site of the spin-1/2 Heisenberg Hamiltonian in MPO form.
-    This is cached.
+def build_spin_ham_mpos(one_site_terms, two_site_terms, S=1 / 2, which=None):
+    """Genereate a spin hamiltonian MPO tensor
 
     Parameters
     ----------
-    j : float
-        (Isotropic) nearest neighbour coupling.
-    bz : float
-        Magnetic field in Z-direction.
+    one_site_terms : sequence of (scalar, operator)
+        The terms that act on a single site, each ``operator`` can be a string
+        suitable to be sent to :func:`spin_operator` or an actual 2d-array.
+    two_site_terms : sequence of (scalar, operator operator)
+        The terms that act on two neighbouring sites, each ``operator`` can be
+        a string suitable to be sent to :func:`spin_operator` or an actual
+        2d-array.
+    S : fraction, optional
+        What size spin to use, defaults to spin-1/2.
+    which : {None, 'L', 'R', 'A'}, optional
+        If ``None``, generate the middle tensor, if 'L' a left-end tensor, if
+        'R' a right-end tensor and if 'A' all three.
+        not ``None``.
 
     Returns
     -------
-    H : numpy.ndarray
-        The tensor, with shape (5, 5, 2, 2).
+    numpy.ndarray{, numpy.ndarray, numpy.ndarray}
     """
-    H = np.zeros((5, 5, 2, 2), dtype=complex)
+    # local dimension
+    D = int(2 * S + 1)
+    # bond dimension
+    B = len(two_site_terms) + 2
 
-    for i, s in enumerate('XYZ'):
-        H[1 + i, 0, :, :] = spin_operator(s)
-        H[-1, 1 + i, :, :] = j * spin_operator(s)
+    H = np.zeros((B, B, D, D), dtype=complex)
 
-    H[0, 0, :, :] = eye(2)
-    H[4, 4, :, :] = eye(2)
-    H[4, 0, :, :] = - bz * spin_operator('Z')
+    # add two-body terms
+    for i, (factor, s1, s2) in enumerate(two_site_terms):
+        if isinstance(s1, str):
+            s1 = spin_operator(s1, S=S)
+        if isinstance(s2, str):
+            s2 = spin_operator(s2, S=S)
+        H[1 + i, 0, :, :] = s1
+        H[-1, 1 + i, :, :] = factor * s2
+
+    # add one-body terms
+    for factor, s in one_site_terms:
+        if isinstance(s, str):
+            s = spin_operator(s, S=S)
+        H[B - 1, 0, :, :] += factor * s
+
+    H[0, 0, :, :] = eye(D)
+    H[B - 1, B - 1, :, :] = eye(D)
 
     make_immutable(H)
+
+    if which == 'L':
+        return H[-1, :, :, :]
+    elif which == 'R':
+        H[:, 0, :, :]
+    elif which == 'A':
+        return H[-1, :, :, :], H, H[:, 0, :, :]
+
     return H
 
 
-def mpo_end_ham_heis_left(j=1.0, bz=0.0):
-    """The left most site of a open boundary conditions Heisenberg
-    matrix product operator.
+class MPOSpinHam:
     """
-    return mpo_site_ham_heis(j=j, bz=bz)[-1, :, :, :]
+    """
+
+    def __init__(self, S=1 / 2):
+        self.S = S
+        self.one_site_terms = []
+        self.two_site_terms = []
+
+    def add_term(self, factor, *operators):
+        """
+        """
+        if len(operators) == 1:
+            self.one_site_terms.append((factor, *operators))
+        elif len(operators) == 2:
+            self.two_site_terms.append((factor, *operators))
+        else:
+            raise NotImplementedError("3-body+ terms are not supported yet.")
+
+    def build(self, n, upper_ind_id='k{}', lower_ind_id='b{}',
+              site_tag_id='i{}', tags=None, bond_name=""):
+        """
+        """
+        left, middle, right = build_spin_ham_mpos(
+            self.one_site_terms, self.two_site_terms, S=self.S, which='A')
+
+        arrays = (left, *[middle] * (n - 2), right)
+
+        return MatrixProductOperator(arrays=arrays, bond_name=bond_name,
+                                     upper_ind_id=upper_ind_id,
+                                     lower_ind_id=lower_ind_id,
+                                     site_tag_id=site_tag_id, tags=tags)
 
 
-def mpo_end_ham_heis_right(j=1.0, bz=0.0):
-    """The right most site of a open boundary conditions Heisenberg
-    matrix product operator.
+def MPO_ham_ising(n, j=1.0, bx=0.0,
+                  upper_ind_id='k{}',
+                  lower_ind_id='b{}',
+                  site_tag_id='i{}',
+                  tags=None,
+                  bond_name=""):
+    """Ising Hamiltonian in matrix product operator form.
     """
-    return mpo_site_ham_heis(j=j, bz=bz)[:, 0, :, :]
+    H = MPOSpinHam(S=1 / 2)
+    H.add_term(j, 'Z', 'Z')
+    H.add_term(-bx, 'X')
+    return H.build(n, site_tag_id=site_tag_id, tags=tags, bond_name=bond_name,
+                   upper_ind_id=upper_ind_id, lower_ind_id=lower_ind_id)
+
+
+def MPO_ham_XY(n, j=1.0, bz=0.0,
+               upper_ind_id='k{}',
+               lower_ind_id='b{}',
+               site_tag_id='i{}',
+               tags=None,
+               bond_name=""):
+    """XY-Hamiltonian in matrix product operator form.
+    """
+    H = MPOSpinHam(S=1 / 2)
+    H.add_term(j, 'X', 'X')
+    H.add_term(j, 'Y', 'Y')
+    H.add_term(-bz, 'Z')
+    return H.build(n, site_tag_id=site_tag_id, tags=tags, bond_name=bond_name,
+                   upper_ind_id=upper_ind_id, lower_ind_id=lower_ind_id)
 
 
 def MPO_ham_heis(n, j=1.0, bz=0.0,
@@ -114,14 +192,10 @@ def MPO_ham_heis(n, j=1.0, bz=0.0,
                  bond_name=""):
     """Heisenberg Hamiltonian in matrix product operator form.
     """
-    arrays = (mpo_end_ham_heis_left(j=j, bz=bz),
-              *[mpo_site_ham_heis(j=j, bz=bz)] * (n - 2),
-              mpo_end_ham_heis_right(j=j, bz=bz))
-
-    HH_mpo = MatrixProductOperator(arrays=arrays,
-                                   upper_ind_id=upper_ind_id,
-                                   lower_ind_id=lower_ind_id,
-                                   site_tag_id=site_tag_id,
-                                   tags=tags,
-                                   bond_name=bond_name)
-    return HH_mpo
+    H = MPOSpinHam(S=1 / 2)
+    H.add_term(j, 'X', 'X')
+    H.add_term(j, 'Y', 'Y')
+    H.add_term(j, 'Z', 'Z')
+    H.add_term(-bz, 'Z')
+    return H.build(n, site_tag_id=site_tag_id, tags=tags, bond_name=bond_name,
+                   upper_ind_id=upper_ind_id, lower_ind_id=lower_ind_id)
