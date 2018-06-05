033877c80d70b6cdf5f7b19f0022fe305542501d
jcmgray <john.gray.14@ucl.ac.uk>
2018-06-05 01:19:32 +0100

    updates to TEBD
    - auto combining consecutive sweeps of same type
    - TEBD.at_times(...)

--- quimb/tensor/tensor_tebd.py
+++ quimb/tensor/tensor_tebd.py
@@ -1,4 +1,5 @@
 import quimb as qu
+from .tensor_core import TensorNetwork
 
 
 class TEBD:
@@ -7,54 +8,74 @@ class TEBD:
     Parameters
     ----------
     p0 : MatrixProductState
-        initial state
+        Initial state.
     ham_int : array_like
         Dense hamiltonian representing the two body interaction. Should have
-        shape ``(d * d, d * d)``, where ``d`` is the physical dimension.
-    dt : float
-        Time step.
-    t0 : float
+        shape ``(d * d, d * d)``, where ``d`` is the physical dimension of
+        ``p0``.
+    dt : float, optional
+        Default time step, cannot be set as well as ``tol``.
+    tol : float, optional
+        Default target error for each evolution, cannot be set as well as
+        ``dt``, which will instead be calculated from the trotter orderm length
+        of time, and hamiltonian norm.
+    t0 : float, optional
         Initial time. Defaults to 0.0.
-    split_opts : dict
+    split_opts : dict, optional
         Compression options applied for splitting after gate application, see
         :func:`~quimb.tensor.tensor_core.tensor_split`.
+
+    See Also
+    --------
+    Evolution
 
     [1] Guifré Vidal, Efficient Classical Simulation of Slightly Entangled
     Quantum Computations, PRL 91, 147902 (2003)
     """
 
-    def __init__(self, p0, ham_int, dt, t0=0.0,
+    def __init__(self, p0, ham_int, dt=None, tol=None, t0=0.0,
                  split_opts=None, progbar=True):
         self.pt = p0.copy()
+        self.pt.canonize(0)
+        self.N = self.pt.nsites
+
+        if isinstance(ham_int, TensorNetwork):
+            raise TypeError("``ham_int`` should be a 2-site array, "
+                            "not a TensorNetwork of any form.")
         self.ham_int = ham_int
-        self.dt = dt
+
         self.t0 = self.t = t0
+
+        if dt and tol:
+            raise ValueError("Can't set default for both ``dt`` and ``tol``.")
+        self.dt = self._dt = dt
+        self.tol = tol
+
         self.progbar = progbar
         self.split_opts = {} if split_opts is None else dict(split_opts)
 
-        self.pt.canonize(0)
-
-        self.N = p0.nsites
-        self.U_ints = {}
+        self._U_ints = {}
         self.err = 0.0
 
-    def choose_time_step(self, err, T, order):
+    T_TOL = 1e-13
+
+    def choose_time_step(self, tol, T, order):
         """Trotter error is ~ (T / dt) * dt **(order + 1). Invert to
         find desired time step, and scale by norm of interaction term.
         """
-        return (1 / qu.norm(self.ham_int, 'fro')) * (err / T) ** (1 / order)
+        return (1 / qu.norm(self.ham_int, 'fro')) * (tol / T) ** (1 / order)
 
     def get_gate(self, dt_frac):
         """Get the unitary gate for fraction of timestep ``dt_frac``, cached.
         """
         try:
-            return self.U_ints[dt_frac]
+            return self._U_ints[dt_frac]
         except KeyError:
-            U = qu.expm(-1.0j * self.dt * dt_frac * self.ham_int)
-            self.U_ints[dt_frac] = U
+            U = qu.expm(-1.0j * self._dt * dt_frac * self.ham_int)
+            self._U_ints[dt_frac] = U
             return U
 
-    def sweep(self, direction, dt_frac, dt=None):
+    def sweep(self, direction, dt_frac, dt=None, queue=False):
         """Perform a single sweep of gates and compression. This shifts the
         orthonognality centre along with the gates as they are applied and
         split.
@@ -65,11 +86,44 @@ class TEBD:
             Which direction to sweep. Right is even bonds, left is odd.
         dt_frac : float
             What fraction of dt substep to take.
+        dt : float, optional
+            Overide the current ``dt`` with a custom value.
         """
 
         # if custom dt set, scale the dt fraction
         if dt is not None:
-            dt_frac *= (dt / self.dt)
+            dt_frac *= (dt / self._dt)
+
+        # ------ automatically combine consecutive sweeps of same time ------ #
+
+        if not hasattr(self, '_queued_sweep'):
+            self._queued_sweep = None
+
+        if queue:
+            # check for queued sweep
+            if self._queued_sweep:
+                # if matches, combine and continue
+                if direction == self._queued_sweep[0]:
+                    self._queued_sweep[1] += dt_frac
+                    return
+                # else perform the old, queue the new
+                else:
+                    new_queued_sweep = [direction, dt_frac]
+                    direction, dt_frac = self._queued_sweep
+                    self._queued_sweep = new_queued_sweep
+
+            # just queue the new sweep
+            else:
+                self._queued_sweep = [direction, dt_frac]
+                return
+
+        # check if need to drain the queue first
+        elif self._queued_sweep:
+            queued_direction, queued_dt_frac = self._queued_sweep
+            self._queued_sweep = None
+            self.sweep(queued_direction, queued_dt_frac, queue=False)
+
+        # ------------------------------------------------------------------- #
 
         U = self.get_gate(dt_frac)
         N = self.N
@@ -77,10 +131,10 @@ class TEBD:
         if direction == 'right':
             # Apply even gates:
             #
-            #     o-<-<-<-<-<-<-<-<-<-<-<
-            #     | | | | | | | | | | | |       >~>~>~>~>~>~>~>~>~>~>~o
-            #     UUU UUU UUU UUU UUU UUU  -->  | | | | | | | | | | | |
-            #     | | | | | | | | | | | |
+            #     o-<-<-<-<-<-<-<-<-<-   -<-<
+            #     | | | | | | | | | |     | |       >~>~>~>~>~>~>~>~>~>~>~o
+            #     UUU UUU UUU UUU UUU ... UUU  -->  | | | | | | | | | | | |
+            #     | | | | | | | | | |     | |
             #      1   2   3   4   5  ==>
             #
             for i in range(0, N - 1, 2):
@@ -91,11 +145,11 @@ class TEBD:
         elif direction == 'left':
             # Apply odd gates:
             #
-            #     >->->->->->->->->->->-o
-            #     | | | | | | | | | | | |       o~<~<~<~<~<~<~<~<~<~<~<
-            #     | UUU UUU UUU UUU UUU |  -->  | | | | | | | | | | | |
-            #     | | | | | | | | | | | |
-            #       <==  4   3   2   1
+            #     >->->-   ->->->->->->->->-o
+            #     | | |     | | | | | | | | |       o~<~<~<~<~<~<~<~<~<~<~<
+            #     | UUU ... UUU UUU UUU UUU |  -->  | | | | | | | | | | | |
+            #     | | |     | | | | | | | | |
+            #           <==  4   3   2   1
             #
             for i in reversed(range(1, N - 1, 2)):
                 self.pt.right_canonize(start=min(N - 1, i + 2), stop=i + 1)
@@ -105,46 +159,130 @@ class TEBD:
             # one extra canonicalization not included in last split
             self.pt.right_canonize_site(1)
 
-    def _step_order2(self, tau=1, dt=None):
+    def _step_order2(self, tau=1, **sweep_opts):
         """Perform a single, second order step.
         """
-        self.sweep('right', tau / 2, dt=dt)
-        self.sweep('left', tau, dt=dt)
-        self.sweep('right', tau / 2, dt=dt)
+        self.sweep('right', tau / 2, **sweep_opts)
+        self.sweep('left', tau, **sweep_opts)
+        self.sweep('right', tau / 2, **sweep_opts)
 
-    def _step_order4(self, dt=None):
+    def _step_order4(self, **sweep_opts):
         """Perform a single, fourth order step.
         """
         tau1 = tau2 = 1 / (4 * 4**(1 / 3))
         tau3 = 1 - 2 * tau1 - 2 * tau2
-        self._step_order2(tau1, dt=dt)
-        self._step_order2(tau2, dt=dt)
-        self._step_order2(tau3, dt=dt)
-        self._step_order2(tau2, dt=dt)
-        self._step_order2(tau1, dt=dt)
+        self._step_order2(tau1, **sweep_opts)
+        self._step_order2(tau2, **sweep_opts)
+        self._step_order2(tau3, **sweep_opts)
+        self._step_order2(tau2, **sweep_opts)
+        self._step_order2(tau1, **sweep_opts)
 
-    def step(self, order=2, dt=None, pbar=None):
+    def step(self, order=2, dt=None, pbar=None, **sweep_opts):
         """Perform a single step of time ``self.dt``.
         """
-        {2: self._step_order2, 4: self._step_order4}[order](dt=dt)
+        {2: self._step_order2,
+         4: self._step_order4}[order](dt=dt, **sweep_opts)
 
-        dt = self.dt if dt is None else dt
+        dt = self._dt if dt is None else dt
         self.t += dt
         self.err += dt ** (order + 1)
 
         if pbar is not None:
             pbar.cupdate(self.t)
+            pbar.set_description(
+                "t={:.4g}, max bond={}".format(self.t, self.pt.max_bond()))
 
-    def update_to(self, t, order=2):
-        """Update the state to time ``t``.
+    def _compute_sweep_dt_tol(self, T, dt, tol, order):
+        # Work out timestep, possibly from target tol, and checking defaults
+        dt = self.dt if (dt is None) else dt
+        tol = self.tol if (tol is None) else tol
+
+        if not (dt or tol):
+            raise ValueError("Must set one of ``dt`` and ``tol``.")
+        if (dt and tol):
+            raise ValueError("Can't set both ``dt`` and ``tol``.")
+
+        if dt is None:
+            self._dt = self.choose_time_step(tol, T - self.t, order)
+        else:
+            self._dt = dt
+
+        return self._dt
+
+    def update_to(self, T, dt=None, tol=None, order=4, progbar=None):
+        """Update the state to time ``T``.
+
+        Parameters
+        ----------
+        T : float
+            The time to evolve to.
+        dt : float, optional
+            Time step to use. Can't be set as well as ``tol``.
+        tol : float, optional
+            Tolerance for whole evolution. Can't be set as well as ``dt``.
+        order : int, optional
+            Trotter order to use.
+        progbar : bool, optional
+            Manually turn the progress bar off.
         """
-        pbar = qu.utils.continuous_progbar(self.t, t) if self.progbar else None
+        if T < self.t - self.T_TOL:
+            raise NotImplementedError
 
-        while self.t < t - 1e-13:
-            # set custom dt if within one step of final time
-            dt = t - self.t if (t - self.t < self.dt) else None
+        self._compute_sweep_dt_tol(T, dt, tol, order)
 
-            self.step(order=order, pbar=pbar, dt=dt)
+        # set up progress bar and start evolution
+        progbar = self.progbar if (progbar is None) else progbar
+        pbar = qu.utils.continuous_progbar(self.t, T) if progbar else None
 
-        if self.progbar:
+        while self.t < T - self.T_TOL:
+            if (T - self.t < self._dt):
+                # set custom dt if within one step of final time
+                dt = T - self.t
+                # also make sure queued sweeps are drained
+                queue = False
+            else:
+                dt = None
+                queue = True
+
+            # perform a step!
+            self.step(order=order, pbar=pbar, dt=dt, queue=queue)
+
+        if progbar:
             pbar.close()
+
+    def at_times(self, ts, dt=None, tol=None, order=4, progbar=None):
+        """Generate the time evolved state at each time in ``ts``.
+
+        Parameters
+        ----------
+        ts : sequence of float
+            The times to evolve to and yield the state at.
+        dt : float, optional
+            Time step to use. Can't be set as well as ``tol``.
+        tol : float, optional
+            Tolerance for whole evolution. Can't be set as well as ``dt``.
+        order : int, optional
+            Trotter order to use.
+        progbar : bool, optional
+            Manually turn the progress bar off.
+        """
+        # convert ts to list, to to calc range and use progress bar
+        ts = sorted(ts)
+        T = ts[-1]
+
+        # need to use dt always so tol applies over whole T sweep
+        dt = self._compute_sweep_dt_tol(T, dt, tol, order)
+
+        # set up progress bar
+        progbar = self.progbar if (progbar is None) else progbar
+        if progbar:
+            ts = qu.utils.progbar(ts)
+
+        for t in ts:
+            self.update_to(t, dt=dt, tol=False, order=order, progbar=False)
+
+            if progbar:
+                ts.set_description(
+                    "t={:.4g}, max bond={}".format(self.t, self.pt.max_bond()))
+
+            yield self.pt
--- tests/test_tensor/test_tensor_tebd.py
+++ tests/test_tensor/test_tensor_tebd.py
@@ -18,17 +18,38 @@ class TestTEBD:
         tebd.sweep('left', 1 / 2)
         assert tebd.pt.count_canonized() == (0, n - 1)
         assert tebd.pt.bond_size(0, 1) > 1
+        assert not tebd._queued_sweep
 
     @pytest.mark.parametrize('order', [2, 4])
-    def test_evolve_obc(self, order):
+    @pytest.mark.parametrize('dt,tol', [
+        (0.0759283, None),
+        (None, 1e-4),
+        (0.0759283, 1e-4),
+        (None, None),
+    ])
+    def test_evolve_obc(self, order, dt, tol):
         n = 10
-        tf = 5
+        tf = 2
         psi0 = qtn.MPS_neel_state(n)
         H_int = qu.ham_heis(2, cyclic=False)
-        tebd = qtn.TEBD(psi0, H_int, dt=0.098741)
+
+        if dt and tol:
+            with pytest.raises(ValueError):
+                qtn.TEBD(psi0, H_int, dt=dt, tol=tol)
+            return
+
+        tebd = qtn.TEBD(psi0, H_int, dt=dt, tol=tol)
+
         tebd.split_opts['cutoff'] = 0.0
+
+        if (dt is None and tol is None):
+            with pytest.raises(ValueError):
+                tebd.update_to(tf, order=order)
+            return
+
         tebd.update_to(tf, order=order)
         assert tebd.t == approx(tf)
+        assert not tebd._queued_sweep
 
         dpsi0 = psi0.to_dense()
         dham = qu.ham_heis(n=n, sparse=True, cyclic=False)
@@ -36,3 +57,18 @@ class TestTEBD:
         evo.update_to(tf)
 
         assert qu.expec(evo.pt, tebd.pt.to_dense()) == approx(1, rel=1e-5)
+
+    @pytest.mark.parametrize('dt,tol', [
+        (0.0759283, None),
+        (None, 1e-5),
+    ])
+    def test_at_times(self, dt, tol):
+        n = 10
+        psi0 = qtn.MPS_neel_state(n)
+        H_int = qu.ham_heis(2, cyclic=False)
+        tebd = qtn.TEBD(psi0, H_int, dt=dt, tol=tol)
+
+        for pt in tebd.at_times([0.1, 0.2, 0.3, 0.4, 0.5]):
+            assert pt.H @ pt == approx(1, rel=1e-5)
+
+        assert tebd.err >= 1e-5
