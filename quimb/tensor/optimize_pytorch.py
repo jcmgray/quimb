eefed90aa7c9661f8ef9450672d7428d1d91a178
matt o'rourke <mattorourke41@gmail.com>
2019-11-18 00:48:09 -0800

    TN: add norm_fn_scalar to opt_pytorch

--- quimb/tensor/optimize_pytorch.py
+++ quimb/tensor/optimize_pytorch.py
@@ -74,12 +74,72 @@ def parse_network_to_torch(tn, constant_tags):
 
 
 class TNOptimizer:
+    """Optimize a tensor network's arrays using pytorch.
+
+    Parameters
+    ----------
+    tn : TensorNetwork
+        The tensor network to optimize, every tensor in this will be treated
+        as a variable.
+    loss_fn : callable
+        The target loss function to minimize. Should accept ``tn`` (backed by
+        pytorch tensors) or the output of ``norm_fn``.
+    norm_fn : callable, optional
+        A function to normalize ``tn`` before being passed to ``loss_fn`` and
+        also to call on the final, optimized tensor network. This offers very
+        general support, so it can be used
+        to enforce any constraints such as normalization or unitarity. The
+        penalty of using this level of generality is that all operations
+        that occur within ``norm_fn`` will be added to the computation graph,
+        and thus differentiated during backpropagation.
+    norm_fn_scalar: callable, optional
+        A function that accepts ``tn`` and computes the scalar ``x`` by which 
+        to multiply each tensor in ``tn`` in order to normalize it. If ``tn``
+        is normalized by simple scalar multiplication of the tensors, it is
+        recommended to use this function instead of ``norm_fn``. In this case,
+        the computation of ``x`` can be performed outside the scope of the
+        differentiable computation graph, while only the scalar multiplication
+        of each tensor by ``x`` is included in the differentiable graph. This
+        can lead to reductions in cpu time and memory usage when compared
+        to ``norm_fn``. Note that only one of ``norm_fn`` and ``norm_fn_scalar``
+        can be specified.
+    loss_constants : dict_like, optional
+        Extra constant arguments to supply to ``loss_fn`` and be converted to
+        tensorflow constant tensors. Can be individual arrays or tensor
+        networks.
+    loss_kwargs : dict_like, optional
+        Other kwargs to supply to ``loss_fn`` that are not arrays or tensor
+        networks.
+    constant_tags : sequence of str, optional
+        Treat any tensors *within* ``tn`` with these tags as constant.
+    optimizer : str, optional
+        Which optimizer to use, default: ``'Adam'``.
+        This should be an optimizer that can be found in the
+        ``torch.optim`` submodule.
+    learning_rate : float, optional
+        The learning rate to apply to use the optimizer with. You can
+        dynamically change this between ``optimize`` calls.
+    loss_target : float, optional
+        If supplied, stop optimizing once this loss is reached.
+    device : string, optional
+        Whether to use ``'cpu'`` or ``'cuda'``, default: ``None``.
+        If ``None``, then gpu (``'cuda'``) will be used if it can be
+        detected, otherwise ``'cpu'`` will be used.
+    progbar : bool, optional
+        Whether to show live progress of the optimization.
+
+
+    General usage examples can be seen in the documentation 
+    for ``tensor.optimize_tensorflow.TNOptimizer``.
+    """
+
 
     def __init__(
         self,
         tn,
         loss_fn,
         norm_fn=None,
+        norm_fn_scalar=None,
         loss_constants=None,
         loss_kwargs=None,
         constant_tags=None,
@@ -99,19 +159,25 @@ class TNOptimizer:
         self.constant_tags = (set() if constant_tags is None
                               else set(constant_tags))
 
-        # use identity if no nomalization required
-        if norm_fn is None:
+        # sort out the normalization between norm_fn or norm_fn_scalar
+        if norm_fn is not None and norm_fn_scalar is not None:
+            raise RuntimeError("""The norm_fn and norm_fn_scalar
+                               kwargs are both specified. Only
+                               one can be used at a time.""")
+        elif norm_fn is None and norm_fn_scalar is None:
+            # use identity if no normalization required
             def norm_fn(x):
                 return x
 
         self.norm_fn = norm_fn
+        self.norm_fn_scalar = norm_fn_scalar
 
         self.loss_constants = {}
         if loss_constants is not None:
             for k, v in loss_constants.items():
                 # check if tensor network supplied
                 if isinstance(v, TensorNetwork):
-                    # convert it to constant tensorflow TN
+                    # convert it to constant pytorch TN
                     self.loss_constants[k] = constant_tn(v)
                 else:
                     self.loss_constants[k] = constant(v)
@@ -122,7 +188,7 @@ class TNOptimizer:
         self.loss = None
         self._n = 0
 
-        # make tensorflow version of network and gather variables etc.
+        # make pytorch version of network and gather variables etc.
         self.tn_opt, self.variables, self.iscomplex = parse_network_to_torch(
             tn, self.constant_tags
         )
@@ -132,7 +198,13 @@ class TNOptimizer:
 
     def closure(self):
         self.optimizer.zero_grad()
-        self.loss = self.loss_fn(self.norm_fn(self.tn_opt))
+        if self.norm_fn is not None:
+            self.loss = self.loss_fn(self.norm_fn(self.tn_opt))
+        else:
+            torch, _ = _TORCH_DEVICE
+            with torch.no_grad():
+                fac = self.norm_fn_scalar(self.tn_opt)
+            self.loss = self.loss_fn(self.tn_opt.multiply_each(fac, inplace=False))
         self.loss.backward()
         return self.loss
 
@@ -164,7 +236,13 @@ class TNOptimizer:
             return (time.time() - self._time_start) > max_time
 
     def _get_tn_opt_numpy(self):
-        tn_opt_numpy = self.norm_fn(self.tn_opt)
+        if self.norm_fn is not None:
+            tn_opt_numpy = self.norm_fn(self.tn_opt)
+        else:
+            torch, _ = _TORCH_DEVICE
+            with torch.no_grad():
+                fac = self.norm_fn_scalar(self.tn_opt)
+            tn_opt_numpy = self.tn_opt.multiply_each(fac, inplace=False)
         tn_opt_numpy.apply_to_arrays(lambda x: x.cpu().detach().numpy())
         return tn_opt_numpy
 
