20d4f53ccae4cb9cddac8255c526001dc5ddcada
Johnnie Gray <johnniemcgray@gmail.com>
2020-02-11 22:15:42 -0800

    travis: petsc install simplify wait 30

--- deps/travis-install-slepc4py.sh
+++ deps/travis-install-slepc4py.sh
@@ -3,5 +3,5 @@ set -ex
 
 export PETSC_CONFIGURE_OPTIONS='--download-mumps --download-scalapack --download-parmetis --download-metis --with-scalar-type=complex'
 pip install Cython
-pip install petsc petsc4py --no-binary :all:
-pip install slepc slepc4py --no-binary :all:
+travis_wait 30 pip install petsc petsc4py --no-binary :all:
+travis_wait 30 pip install slepc slepc4py --no-binary :all:
