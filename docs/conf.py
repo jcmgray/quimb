34aad746921df2f71108e7bcc903897030b6cfd3
Johnnie Gray <johnniemcgray@gmail.com>
2023-03-23 15:46:43 -0700

    simplify conf.py, fix release source code links [no ci]

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,14 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import sys
 import os
+import sys
 sys.path.append(os.path.abspath("./_pygments"))
 
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'quimb'
 copyright = '2015-2023, Johnnie Gray'
@@ -38,10 +28,8 @@ except ImportError:
 version = '.'.join(release.split('.')[:2])
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.extlinks',
@@ -86,11 +74,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
-
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 html_theme_options = {
@@ -111,9 +96,6 @@ html_theme_options = {
 pygments_style = '_pygments_light.MarianaLight'
 pygments_dark_style = "_pygments_dark.MarianaDark"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ["my-styles.css"]
 html_favicon = "_static/quimb.ico"
@@ -169,6 +151,6 @@ def linkcode_resolve(domain, info):
         )
     else:
         return (
-            f"https://github.com/pydata/quimb/blob/"
+            f"https://github.com/jcmgray/quimb/blob/"
             f"v{quimb.__version__}/quimb/{fn}{linespec}"
         )
