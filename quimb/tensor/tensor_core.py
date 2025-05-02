b8d0e74dc161b78d7e54e81a698dd9cab046e625
Johnnie Gray <johnniemcgray@gmail.com>
2025-05-01 18:12:32 -0700

    tweak example docstring [skip ci]

--- quimb/tensor/tensor_core.py
+++ quimb/tensor/tensor_core.py
@@ -2067,9 +2067,11 @@ class Tensor:
         Examples
         --------
 
-            t = qtn.rand_tensor((2, 3, 4), ('a', 'b', 'c'))
-            t.new_ind_pair_diag_('b', 'x', 'y')
-            # Tensor(shape=(2, 3, 3, 4), inds=('a', 'x', 'y', 'c'), tags=oset([]))
+        ```python
+        t = qtn.rand_tensor((2, 3, 4), ('a', 'b', 'c'))
+        t.new_ind_pair_diag_('b', 'x', 'y')
+        # Tensor(shape=(2, 3, 3, 4), inds=('a', 'x', 'y', 'c'), tags=oset([]))
+        ```
         """
         t = self if inplace else self.copy()
 
