29acfab0009990dd96dd4b00f0e82aa81bf8dd9d
sijingdu <sdu2@caltech.edu>
2025-09-19 15:09:13 -0700

    format the test file and add @require_symmray to test function

--- tests/test_tensor/test_fmps_fitting.py
+++ tests/test_tensor/test_fmps_fitting.py
@@ -1,7 +1,7 @@
-import pytest
 import importlib
 
 import numpy as np
+import pytest
 import symmray as sr
 
 requires_symmray = pytest.mark.skipif(
@@ -13,29 +13,68 @@ requires_symmray = pytest.mark.skipif(
 # fPEPS parameters
 Lx = int(4)
 Ly = int(4)
-symmetry = 'Z2'
+symmetry = "Z2"
 D = 4
 # Load PEPS
-fpeps = sr.PEPS_fermionic_rand(symmetry, Lx, Ly, bond_dim=D, phys_dim=4, seed=42)
+fpeps = sr.PEPS_fermionic_rand(
+    symmetry, Lx, Ly, bond_dim=D, phys_dim=4, seed=42
+)
 fpeps.equalize_norms_()
 for ts in fpeps.tensors:
     ts.data.phase_sync(inplace=True)
 fpeps_norm = fpeps.make_norm()
 # benchmark_norm = fpeps_norm.contract_boundary_from_xmax(xrange=(0, Lx-1), max_bond=256, cutoff=0.0, mode='direct').contract()
-benchmark_norm = np.float64(9.347604511732736e+18)
+benchmark_norm = np.float64(9.347604511732736e18)
 
-@pytest.mark.parametrize("from_which", ['xmin', 'xmax', 'ymin', 'ymax'])
+@requires_symmray
+@pytest.mark.parametrize("from_which", ["xmin", "xmax", "ymin", "ymax"])
 def test_fmps_mpo_fitting(from_which):
-    print(f'Benchmark norm: {benchmark_norm}')
-    print('Boundary fMPS-MPO fitting contraction test:')
+    print(f"Benchmark norm: {benchmark_norm}")
+    print("Boundary fMPS-MPO fitting contraction test:")
     # contraction bond dimension
     chi = 128
-    if from_which == 'xmin':
-        print('xmin:')
-        c_xmin_0 = fpeps_norm.contract_boundary_from_xmin(xrange=(0, 1), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_xmin_1 = fpeps_norm.contract_boundary_from_xmin(xrange=(0, 2), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_xmin_2 = fpeps_norm.contract_boundary_from_xmin(xrange=(0, 1), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
-        c_xmin_3 = fpeps_norm.contract_boundary_from_xmin(xrange=(0, 2), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
+    if from_which == "xmin":
+        print("xmin:")
+        c_xmin_0 = fpeps_norm.contract_boundary_from_xmin(
+            xrange=(0, 1),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_xmin_1 = fpeps_norm.contract_boundary_from_xmin(
+            xrange=(0, 2),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_xmin_2 = fpeps_norm.contract_boundary_from_xmin(
+            xrange=(0, 1),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
+        c_xmin_3 = fpeps_norm.contract_boundary_from_xmin(
+            xrange=(0, 2),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
         print(c_xmin_0, np.allclose(c_xmin_0, benchmark_norm, rtol=1e-4))
         print(c_xmin_1, np.allclose(c_xmin_1, benchmark_norm, rtol=1e-4))
         print(c_xmin_2, np.allclose(c_xmin_2, benchmark_norm, rtol=1e-4))
@@ -44,12 +83,48 @@ def test_fmps_mpo_fitting(from_which):
         assert np.allclose(c_xmin_1, benchmark_norm, rtol=1e-4)
         assert np.allclose(c_xmin_2, benchmark_norm, rtol=1e-4)
         assert np.allclose(c_xmin_3, benchmark_norm, rtol=1e-4)
-    elif from_which == 'xmax':
-        print('xmax:')
-        c_xmax_0 = fpeps_norm.contract_boundary_from_xmax(xrange=(2, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_xmax_1 = fpeps_norm.contract_boundary_from_xmax(xrange=(1, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_xmax_2 = fpeps_norm.contract_boundary_from_xmax(xrange=(2, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
-        c_xmax_3 = fpeps_norm.contract_boundary_from_xmax(xrange=(1, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
+    elif from_which == "xmax":
+        print("xmax:")
+        c_xmax_0 = fpeps_norm.contract_boundary_from_xmax(
+            xrange=(2, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_xmax_1 = fpeps_norm.contract_boundary_from_xmax(
+            xrange=(1, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_xmax_2 = fpeps_norm.contract_boundary_from_xmax(
+            xrange=(2, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
+        c_xmax_3 = fpeps_norm.contract_boundary_from_xmax(
+            xrange=(1, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
         print(c_xmax_0, np.allclose(c_xmax_0, benchmark_norm, rtol=1e-4))
         print(c_xmax_1, np.allclose(c_xmax_1, benchmark_norm, rtol=1e-4))
         print(c_xmax_2, np.allclose(c_xmax_2, benchmark_norm, rtol=1e-4))
@@ -58,12 +133,48 @@ def test_fmps_mpo_fitting(from_which):
         assert np.allclose(c_xmax_1, benchmark_norm, rtol=1e-4)
         assert np.allclose(c_xmax_2, benchmark_norm, rtol=1e-4)
         assert np.allclose(c_xmax_3, benchmark_norm, rtol=1e-4)
-    elif from_which == 'ymin':
-        print('ymin:')
-        c_ymin_0 = fpeps_norm.contract_boundary_from_ymin(yrange=(0, 1), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_ymin_1 = fpeps_norm.contract_boundary_from_ymin(yrange=(0, 2), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_ymin_2 = fpeps_norm.contract_boundary_from_ymin(yrange=(0, 1), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
-        c_ymin_3 = fpeps_norm.contract_boundary_from_ymin(yrange=(0, 2), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
+    elif from_which == "ymin":
+        print("ymin:")
+        c_ymin_0 = fpeps_norm.contract_boundary_from_ymin(
+            yrange=(0, 1),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_ymin_1 = fpeps_norm.contract_boundary_from_ymin(
+            yrange=(0, 2),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_ymin_2 = fpeps_norm.contract_boundary_from_ymin(
+            yrange=(0, 1),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
+        c_ymin_3 = fpeps_norm.contract_boundary_from_ymin(
+            yrange=(0, 2),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
         print(c_ymin_0, np.allclose(c_ymin_0, benchmark_norm, rtol=1e-4))
         print(c_ymin_1, np.allclose(c_ymin_1, benchmark_norm, rtol=1e-4))
         print(c_ymin_2, np.allclose(c_ymin_2, benchmark_norm, rtol=1e-4))
@@ -72,12 +183,48 @@ def test_fmps_mpo_fitting(from_which):
         assert np.allclose(c_ymin_1, benchmark_norm, rtol=1e-4)
         assert np.allclose(c_ymin_2, benchmark_norm, rtol=1e-4)
         assert np.allclose(c_ymin_3, benchmark_norm, rtol=1e-4)
-    elif from_which == 'ymax':
-        print('ymax:')
-        c_ymax_0 = fpeps_norm.contract_boundary_from_ymax(yrange=(2, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_ymax_1 = fpeps_norm.contract_boundary_from_ymax(yrange=(1, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=6).contract()
-        c_ymax_2 = fpeps_norm.contract_boundary_from_ymax(yrange=(2, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
-        c_ymax_3 = fpeps_norm.contract_boundary_from_ymax(yrange=(1, 3), max_bond=chi, cutoff=0.0, mode='fit', tol=1e-5, tn_fit='zipup', bsz=2, max_iterations=5).contract()
+    elif from_which == "ymax":
+        print("ymax:")
+        c_ymax_0 = fpeps_norm.contract_boundary_from_ymax(
+            yrange=(2, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_ymax_1 = fpeps_norm.contract_boundary_from_ymax(
+            yrange=(1, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=6,
+        ).contract()
+        c_ymax_2 = fpeps_norm.contract_boundary_from_ymax(
+            yrange=(2, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
+        c_ymax_3 = fpeps_norm.contract_boundary_from_ymax(
+            yrange=(1, 3),
+            max_bond=chi,
+            cutoff=0.0,
+            mode="fit",
+            tol=1e-5,
+            tn_fit="zipup",
+            bsz=2,
+            max_iterations=5,
+        ).contract()
         print(c_ymax_0, np.allclose(c_ymax_0, benchmark_norm, rtol=1e-4))
         print(c_ymax_1, np.allclose(c_ymax_1, benchmark_norm, rtol=1e-4))
         print(c_ymax_2, np.allclose(c_ymax_2, benchmark_norm, rtol=1e-4))
