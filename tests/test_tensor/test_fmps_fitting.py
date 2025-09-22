632e910949dd6df662075a4c65587e65af0bb852
sijingdu <sdu2@caltech.edu>
2025-09-21 22:41:43 -0700

    correct bugs in pytest file

--- tests/test_tensor/test_fmps_fitting.py
+++ tests/test_tensor/test_fmps_fitting.py
@@ -2,33 +2,38 @@ import importlib
 
 import numpy as np
 import pytest
-import symmray as sr
 
 requires_symmray = pytest.mark.skipif(
     importlib.util.find_spec("symmray") is None,
     reason="symmray not installed",
 )
 
-
-# fPEPS parameters
-Lx = int(4)
-Ly = int(4)
-symmetry = "Z2"
-D = 4
-# Load PEPS
-fpeps = sr.PEPS_fermionic_rand(
-    symmetry, Lx, Ly, bond_dim=D, phys_dim=4, seed=42
-)
-fpeps.equalize_norms_()
-for ts in fpeps.tensors:
-    ts.data.phase_sync(inplace=True)
-fpeps_norm = fpeps.make_norm()
-# benchmark_norm = fpeps_norm.contract_boundary_from_xmax(xrange=(0, Lx-1), max_bond=256, cutoff=0.0, mode='direct').contract()
-benchmark_norm = np.float64(9.347604511732736e18)
+@requires_symmray
+@pytest.fixture
+def get_fpeps_and_norm():
+    import symmray as sr
+    # fPEPS parameters
+    Lx = int(4)
+    Ly = int(4)
+    symmetry = "Z2"
+    D = 4
+    # Load PEPS
+    fpeps = sr.PEPS_fermionic_rand(
+        symmetry, Lx, Ly, bond_dim=D, phys_dim=4, seed=42
+    )
+    fpeps.equalize_norms_()
+    for ts in fpeps.tensors:
+        ts.data.phase_sync(inplace=True)
+    fpeps_norm = fpeps.make_norm()
+    # benchmark_norm = fpeps_norm.contract_boundary_from_xmax(xrange=(0, Lx-1), max_bond=256, cutoff=0.0, mode='direct').contract()
+    benchmark_norm = np.float64(9.347604511732736e18)
+    return fpeps_norm, benchmark_norm
 
 @requires_symmray
 @pytest.mark.parametrize("from_which", ["xmin", "xmax", "ymin", "ymax"])
-def test_fmps_mpo_fitting(from_which):
+def test_fmps_mpo_fitting(from_which, get_fpeps_and_norm):
+    
+    fpeps_norm, benchmark_norm = get_fpeps_and_norm
     print(f"Benchmark norm: {benchmark_norm}")
     print("Boundary fMPS-MPO fitting contraction test:")
     # contraction bond dimension
